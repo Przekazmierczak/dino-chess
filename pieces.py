bb52b8258b4078c188c7b576d617a013b6c9aeab
Przekazmierczak <prze.kazmierczak@gmail.com>
2024-02-24 16:44:05 +0100

    finish pieces classes

--- pieces.py
+++ pieces.py
@@ -3,47 +3,43 @@ class Pawn:
         self.player = player
         self.row, self.column = location
         self.moved = False
-        self.possible_moves = self.check_possible_moves()
-        self.possible_attacks = self.check_possible_attacks()
 
     def check_possible_moves(self):
-        moves = []
         direction = 1 if self.player == "white" else -1
+        moves = []
+        attacks = []
 
         move_by_one = self.row + direction
-        if move_by_one in range(0, 7):
+
+        if move_by_one in range(0, 7) and board[move_by_one][self.column] is None:
             moves.append((move_by_one, self.column))
 
-        move_by_two = self.row + 2 * direction
-        if not self.moved and move_by_two in range(0, 7):
-            moves.append((move_by_two, self.column))
-        return moves
+            move_by_two = self.row + 2 * direction
+            if not self.moved and move_by_two in range(0, 7) and board[move_by_two][self.column] is None:
+                moves.append((move_by_two, self.column))
 
-    def check_possible_attacks(self):
-        attacks = []
-        direction = 1 if self.player == "white" else -1
-
-        move_by_one = self.row + direction
         attack_right = self.column + 1
-        if move_by_one in range(0, 7) and attack_right in range(0, 7):
-            attacks.append((self.row + direction, attack_right))
+        if move_by_one in range(0, 7) and attack_right in range(0, 7) and board[move_by_one][attack_right] is not None and board[move_by_one][attack_right].player != self.player:
+            attacks.append((move_by_one, attack_right))
         
         attack_left = self.column - 1
-        if move_by_one in range(0, 7) and attack_left in range(0, 7):
-            attacks.append((self.row + direction, attack_left))
-        return attacks
+        if move_by_one in range(0, 7) and attack_left in range(0, 7) and board[move_by_one][attack_right] is not None and board[move_by_one][attack_right].player != self.player:
+            attacks.append((move_by_one, attack_left))
+
+        return (moves, attacks)
+
 
 class Rook:
     def __init__(self, player, location):
         self.player = player
         self.row, self.column = location
         self.moved = False
-        self.possible_moves = self.check_possible_moves()
-        self.possible_attacks = self.check_possible_attacks()
 
     def check_possible_moves(self):
-        moves = []
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        moves = []
+        attacks = []
+
         for direction in directions:
             distance = 1
             while True:
@@ -53,28 +49,123 @@ class Rook:
                 if new_row not in range(0, 7) or new_column not in range(0, 7):
                     break
 
+                if board[new_row][new_column] is not None:
+                    if board[new_row][new_column].player is not self.player:
+                        attacks.append(((new_row, new_column)))
+                    break
+
                 moves.append((new_row, new_column))
                 distance += 1
-        return moves
-        
-    def check_possible_attacks(self):
-        self.possible_attacks = self.possible_moves
+
+        return (moves, attacks)
+    
 
 class Knight:
     def __init__(self, player, location):
-        pass
+        self.player = player
+        self.row, self.column = location
+        self.moved = False
+    
+    def check_possible_moves(self):
+        directions = [(2, 1), (-2, 1), (2, -1), (-2, -1), (1, 2), (-1, 2), (1, -2), (-1, -2)]
+        moves = []
+        attacks = []
+
+        for direction in directions:
+            new_row = self.row + direction[0]
+            new_column = self.column + direction[1]
+
+            if new_row in range(0, 7) and new_column in range(0, 7):
+                if board[new_row][new_column] is None:
+                    moves.append((new_row, new_column))
+                elif board[new_row][new_column].player is not self.player:
+                    attacks.append((new_row, new_column))
+
+        return (moves, attacks)
 
 class Bishop:
     def __init__(self, player, location):
-        pass
+        self.player = player
+        self.row, self.column = location
+        self.moved = False
+    
+    def check_possible_moves(self):
+        directions = [(1, 1), (1, -1), (-1, 1), (-1, -1)]
+        moves = []
+        attacks = []
 
-class King:
-    def __init__(self, player, location):
-        pass
+        for direction in directions:
+            distance = 1
+            while True:
+                new_row = self.row + distance * direction[0]
+                new_column = self.column + distance * direction[1]
+
+                if new_row not in range(0, 7) or new_column not in range(0, 7):
+                    break
+
+                if board[new_row][new_column] is not None:
+                    if board[new_row][new_column].player is not self.player:
+                        attacks.append(((new_row, new_column)))
+                    break
+
+                moves.append((new_row, new_column))
+                distance += 1
+
+        return (moves, attacks)
 
 class Queen:
     def __init__(self, player, location):
-        pass
+        self.player = player
+        self.row, self.column = location
+        self.moved = False
+    
+    def check_possible_moves(self):
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+        moves = []
+        attacks = []
+
+        for direction in directions:
+            distance = 1
+            while True:
+                new_row = self.row + distance * direction[0]
+                new_column = self.column + distance * direction[1]
+
+                if new_row not in range(0, 7) or new_column not in range(0, 7):
+                    break
+
+                if board[new_row][new_column] is not None:
+                    if board[new_row][new_column].player is not self.player:
+                        attacks.append(((new_row, new_column)))
+                    break
+
+                moves.append((new_row, new_column))
+                distance += 1
+
+        return (moves, attacks)
+
+class King:
+    def __init__(self, player, location):
+        self.player = player
+        self.row, self.column = location
+        self.moved = False
+
+    def check_possible_moves(self):
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+        moves = []
+        attacks = []
+
+        for direction in directions:
+            new_row = self.row + direction[0]
+            new_column = self.column + direction[1]
+
+            if new_row in range(0, 7) and new_column in range(0, 7):
+                if board[new_row][new_column] is None:
+                    moves.append((new_row, new_column))
+                elif board[new_row][new_column].player is not self.player:
+                    attacks.append((new_row, new_column))
+
+        return (moves, attacks)
+    
 
 # initiate new board
 
@@ -89,4 +180,7 @@ for player, row in [("white", 0), ("white", 1), ("black", 6), ("black", 7)]:
         for column in range(8):
             board[row][column] = Pawn(player, (row, column))
 
-print(board[0][0].possible_moves)
+board[4][1] = Knight("black", (4,1))
+# board[1][0] = None
+
+print(board[0][3].check_possible_moves())
