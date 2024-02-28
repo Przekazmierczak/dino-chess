d774eff095a15be4d16a03b2d397a2e44bfd550b
Przekazmierczak <prze.kazmierczak@gmail.com>
2024-02-28 21:44:07 +0100

    add models

--- chess_django/table/models.py
+++ chess_django/table/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    total_moves = models.IntegerField()
+    board = models.TextField()
+    player = models.CharField(max_length=1)
+    castling = models.CharField(max_length=4)
+    soft_moves = models.IntegerField()
+
+class Games(models.Model):
+    game_history = models.ManyToManyField(Board)
