e4a3279f57c25f77f4cb08be5d0bcc9c5a00cb02
Przekazmierczak <prze.kazmierczak@gmail.com>
2024-04-01 20:29:06 +0200

    prepare for enpassant and castling tests, fix tests comments

--- chess_django/table/test_piece_class.py
+++ chess_django/table/test_piece_class.py
@@ -10,7 +10,7 @@ class BasicMovements(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.board = [[None for _ in range(ROWS)] for _ in range(COLS)]
-        cls.turn, cls.opponents_attacks, cls.checkin_pieces, cls.pinned_pieces = "black", set(), {}, {}
+        cls.turn, cls.opponents_attacks, cls.checkin_pieces, cls.pinned_pieces, cls.castling, cls.enpassant = "black", set(), {}, {}, "____", None
 
         cls.pieces_positions = [("pawn", "white", (1, 0)), ("pawn", "white", (2, 4)), ("pawn", "white", (1, 6)),
                                 ("pawn", "black", (6, 3)), ("pawn", "black", (5, 4)), ("pawn", "black", (6, 7)),
@@ -26,86 +26,86 @@ class BasicMovements(unittest.TestCase):
 
     
     def get_moves(self, row, col):
-        moves, attacks = self.board[row][col].check_piece_possible_moves(self, self.opponents_attacks, self.checkin_pieces, self.pinned_pieces)
+        moves, attacks = self.board[row][col].check_piece_possible_moves(self, self.opponents_attacks, self.checkin_pieces, self.pinned_pieces, self.castling, self.enpassant)
         return (set(moves), set(attacks))
 
     def test_basic_movements_pawn_1_0(self):
-        """Examine the movements of the pawn located on square (1, 0)."""
+        """Examine the movements of the pawn located on square (1, 0) - Basic movements test."""
         self.assertEqual(self.get_moves(1, 0), (set(), set()))
 
     def test_basic_movements_pawn_2_3(self):
-        """Examine the movements of the pawn located on square (2, 3)."""
+        """Examine the movements of the pawn located on square (2, 3) - Basic movements test."""
         self.board[2][4].moved = True
         self.assertEqual(self.get_moves(2, 4), ({(3, 4)}, set()))
 
     def test_basic_movements_pawn_1_6(self):
-        """Examine the movements of the pawn located on square (1, 6)."""
+        """Examine the movements of the pawn located on square (1, 6) - Basic movements test."""
         self.assertEqual(self.get_moves(1, 6), ({(2, 6), (3, 6)}, set()))
 
     def test_basic_movements_pawn_6_3(self):
-        """Examine the movements of the pawn located on square (6, 3)."""
+        """Examine the movements of the pawn located on square (6, 3) - Basic movements test."""
         self.assertEqual(self.get_moves(6, 3), ({(5, 3)}, set()))
 
     def test_basic_movements_pawn_5_4(self):
-        """Examine the movements of the pawn located on square (5, 4)."""
+        """Examine the movements of the pawn located on square (5, 4) - Basic movements test."""
         self.board[5][4].moved = True
         self.assertEqual(self.get_moves(5, 4), ({(4, 4)}, {(4, 3)}))
 
     def test_basic_movements_pawn_6_7(self):
-        """Examine the movements of the pawn located on square (6, 7)."""
+        """Examine the movements of the pawn located on square (6, 7) - Basic movements test."""
         self.assertEqual(self.get_moves(6, 7), (set(), set()))
 
     def test_basic_movements_rook_0_0(self):
-        """Examine the movements of the rook located on square (0, 0)."""
+        """Examine the movements of the rook located on square (0, 0) - Basic movements test."""
         self.assertEqual(self.get_moves(0, 0), ({(0, 1), (0, 2), (0, 3)}, set()))
         
     def test_basic_movements_rook_5_7(self):
-        """Examine the movements of the rook located on square (5, 7)."""
+        """Examine the movements of the rook located on square (5, 7) - Basic movements test."""
         self.assertEqual(self.get_moves(5, 7), ({(4, 7), (3, 7), (2, 7), (1, 7), (0, 7), (5, 6)}, {(6, 7), (5, 5)}))
 
     def test_basic_movements_rook_7_0(self):
-        """Examine the movements of the rook located on square (7, 0)."""
+        """Examine the movements of the rook located on square (7, 0) - Basic movements test."""
         self.assertEqual(self.get_moves(7, 0), ({(6, 0), (5, 0), (4, 0), (3, 0)}, {(2, 0)}))
 
     def test_basic_movements_rook_7_4(self):
-        """Examine the movements of the rook located on square (7, 4)."""
+        """Examine the movements of the rook located on square (7, 4) - Basic movements test."""
         self.assertEqual(self.get_moves(7, 4), ({(7, 5), (7, 6), (7, 7)}, {(6, 4)}))
 
     def test_basic_movements_knight_2_0(self):
-        """Examine the movements of the knight located on square (2, 0)."""
+        """Examine the movements of the knight located on square (2, 0) - Basic movements test."""
         self.assertEqual(self.get_moves(2, 0), ({(0, 1), (3, 2), (1, 2)}, set()))
 
     def test_basic_movements_knight_6_4(self):
-        """Examine the movements of the knight located on square (6, 4)."""
+        """Examine the movements of the knight located on square (6, 4) - Basic movements test."""
         self.assertEqual(self.get_moves(6, 4), ({(4, 5), (7, 6), (5, 6), (5, 2)}, {(7,2)}))
 
     def test_basic_movements_knight_7_1(self):
-        """Examine the movements of the knight located on square (7, 1)."""
+        """Examine the movements of the knight located on square (7, 1) - Basic movements test."""
         self.assertEqual(self.get_moves(7, 1), ({(5, 2), (5, 0)}, set()))
 
     def test_basic_movements_knight_5_5(self):
-        """Examine the movements of the knight located on square (5, 5)."""
+        """Examine the movements of the knight located on square (5, 5) - Basic movements test."""
         self.assertEqual(self.get_moves(5, 5), ({(7, 6), (3, 6), (3, 4), (4, 7)}, {(4, 3)}))
 
     def test_basic_movements_bishop_4_1(self):
-        """Examine the movements of the bishop located on square (4, 1)."""
+        """Examine the movements of the bishop located on square (4, 1) - Basic movements test."""
         self.assertEqual(self.get_moves(4, 1), ({(5, 2), (5, 0), (3, 2), (2, 3), (1, 4), (0, 5), (3, 0)}, {(6, 3)}))
     
     def test_basic_movements_bishop_7_2(self):
-        """Examine the movements of the bishop located on square (7, 2)."""
+        """Examine the movements of the bishop located on square (7, 2) - Basic movements test."""
         self.assertEqual(self.get_moves(7, 2), ({(6, 1), (5, 0)}, set()))
     
     def test_basic_movements_queen_4_3(self):
-        """Examine the movements of the queen located on square (4, 3)."""
+        """Examine the movements of the queen located on square (4, 3) - Basic movements test."""
         self.assertEqual(self.get_moves(4, 3), ({(5, 3), (3, 3), (2, 3), (1, 3), (0, 3), (4, 4), (4, 5), (4, 6), (4, 7), (4, 2), (5, 2), (6, 1), (3, 4), (2, 5), (3, 2), (2, 1)},
                                                 {(6, 3), (5, 4), (7, 0)}))
 
     def test_basic_movements_king_0_4(self):
-        """Examine the movements of the bishop located on square (0, 4)."""
+        """Examine the movements of the king located on square (0, 4) - Basic movements test."""
         self.assertEqual(self.get_moves(0, 4), ({(1, 4), (0, 5), (0, 3), (1, 5), (1, 3)}, set()))
 
     def test_basic_movements_king_7_3(self):
-        """Examine the movements of the bishop located on square (6, 6)."""
+        """Examine the movements of the king located on square (7, 3) - Basic movements test."""
         self.assertEqual(self.get_moves(7, 3), ({(6, 2)}, {(6, 4)}))
 
 class BlockTheKing(unittest.TestCase):
@@ -115,7 +115,7 @@ class BlockTheKing(unittest.TestCase):
     def setUpClass(cls):
         cls.board = [[None for _ in range(ROWS)] for _ in range(COLS)]
         cls.possible_moves = [[None for _ in range(ROWS)] for _ in range(COLS)]
-        cls.turn, cls.opponents_attacks, cls.checkin_pieces, cls.pinned_pieces = "black", set(), {}, {}
+        cls.turn, cls.opponents_attacks, cls.checkin_pieces, cls.pinned_pieces, cls.castling, cls.enpassant = "black", set(), {}, {}, "____", None
 
         cls.pieces_positions = [("pawn", "white", (1, 2)), ("rook", "white", (1, 5)), ("knight", "white", (0, 5)), ("bishop", "white", (2, 6)),
                                 ("queen", "white", (3, 7)), ("king", "white", (0, 0)), ("king", "black", (3, 4))]
@@ -140,11 +140,11 @@ class BlockTheKing(unittest.TestCase):
                     cls.possible_moves[row][col] = cls.get_moves(cls, row, col)
     
     def get_moves(self, row, col):
-        moves, attacks = self.board[row][col].check_piece_possible_moves(self, self.opponents_attacks, self.checkin_pieces, self.pinned_pieces)
+        moves, attacks = self.board[row][col].check_piece_possible_moves(self, self.opponents_attacks, self.checkin_pieces, self.pinned_pieces, self.castling, self.enpassant)
         return (set(moves), set(attacks))
 
     def test_block_the_king_king_3_4(self):
-        """Examine the movements of the king located on square (3, 4)."""
+        """Examine the movements of the king located on square (3, 4) - Block the king test."""
         self.assertEqual(self.get_moves(3, 4), ({(4, 3)}, set()))
 
 class AbsolutePin(unittest.TestCase):
@@ -154,7 +154,7 @@ class AbsolutePin(unittest.TestCase):
     def setUpClass(cls):
         cls.board = [[None for _ in range(ROWS)] for _ in range(COLS)]
         cls.possible_moves = [[None for _ in range(ROWS)] for _ in range(COLS)]
-        cls.turn, cls.opponents_attacks, cls.checkin_pieces, cls.pinned_pieces = "black", set(), {}, {}
+        cls.turn, cls.opponents_attacks, cls.checkin_pieces, cls.pinned_pieces, cls.castling, cls.enpassant = "black", set(), {}, {}, "____", None
 
         cls.pieces_positions = [("pawn", "black", (2, 5)),
                                 ("rook", "white", (3, 0)),
@@ -183,19 +183,19 @@ class AbsolutePin(unittest.TestCase):
                     cls.possible_moves[row][col] = cls.get_moves(cls, row, col)
     
     def get_moves(self, row, col):
-        moves, attacks = self.board[row][col].check_piece_possible_moves(self, self.opponents_attacks, self.checkin_pieces, self.pinned_pieces)
+        moves, attacks = self.board[row][col].check_piece_possible_moves(self, self.opponents_attacks, self.checkin_pieces, self.pinned_pieces, self.castling, self.enpassant)
         return (set(moves), set(attacks))
 
     def test_absolute_pin_pawn_2_5(self):
-        """Examine the movements of the pawn located on square (2, 5)."""
+        """Examine the movements of the pawn located on square (2, 5) - Absolute pin test."""
         self.assertEqual(self.get_moves(2, 5), (set(), {(1, 6)}))
 
     def test_absolute_pin_knight_3_2(self):
-        """Examine the movements of the knight located on square (3, 2)."""
+        """Examine the movements of the knight located on square (3, 2) - Absolute pin test."""
         self.assertEqual(self.get_moves(3, 2), (set(), set()))
 
     def test_absolute_pin_bishop_5_2(self):
-        """Examine the movements of the bishop located on square (5, 2)."""
+        """Examine the movements of the bishop located on square (5, 2) - Absolute pin test."""
         self.assertEqual(self.get_moves(5, 2), ({(6, 1), (4, 3)}, {(7, 0)}))
 
 class Check(unittest.TestCase):
@@ -205,7 +205,7 @@ class Check(unittest.TestCase):
     def setUpClass(cls):
         cls.board = [[None for _ in range(ROWS)] for _ in range(COLS)]
         cls.possible_moves = [[None for _ in range(ROWS)] for _ in range(COLS)]
-        cls.turn, cls.opponents_attacks, cls.checkin_pieces, cls.pinned_pieces = "black", set(), {}, {}
+        cls.turn, cls.opponents_attacks, cls.checkin_pieces, cls.pinned_pieces, cls.castling, cls.enpassant = "black", set(), {}, {}, "____", None
 
         cls.pieces_positions = [("pawn", "black", (4, 0)), ("pawn", "black", (4, 2)), ("pawn", "black", (3, 7)),
                                 ("rook", "black", (7, 5)),
@@ -234,39 +234,39 @@ class Check(unittest.TestCase):
                     cls.possible_moves[row][col] = cls.get_moves(cls, row, col)
     
     def get_moves(self, row, col):
-        moves, attacks = self.board[row][col].check_piece_possible_moves(self, self.opponents_attacks, self.checkin_pieces, self.pinned_pieces)
+        moves, attacks = self.board[row][col].check_piece_possible_moves(self, self.opponents_attacks, self.checkin_pieces, self.pinned_pieces, self.castling, self.enpassant)
         return (set(moves), set(attacks))
 
     def test_check_pawn_4_0(self):
-        """Examine the movements of the pawn located on square (4, 0)."""
+        """Examine the movements of the pawn located on square (4, 0) - Check test."""
         self.assertEqual(self.get_moves(4, 0), (set(), {(3, 1)}))
 
     def test_check_pawn_4_2(self):
-        """Examine the movements of the pawn located on square (4, 2)."""
+        """Examine the movements of the pawn located on square (4, 2) - Check test."""
         self.assertEqual(self.get_moves(4, 2), ({(3, 2)}, {(3,1)}))
 
     def test_check_pawn_3_7(self):
-        """Examine the movements of the pawn located on square (3, 7)."""
+        """Examine the movements of the pawn located on square (3, 7) - Check test."""
         self.assertEqual(self.get_moves(3, 7), (set(), set()))
 
     def test_check_rook_7_5(self):
-        """Examine the movements of the rook located on square (7, 5)."""
+        """Examine the movements of the rook located on square (7, 5) - Check test."""
         self.assertEqual(self.get_moves(7, 5), ({(3, 5)}, set()))
 
     def test_check_knight_1_2(self):
-        """Examine the movements of the knight located on square (1, 2)."""
+        """Examine the movements of the knight located on square (1, 2) - Check test."""
         self.assertEqual(self.get_moves(1, 2), ({(3, 3)}, {(3, 1)}))
 
     def test_check_bishop_1_3(self):
-        """Examine the movements of the bishop located on square (1, 3)."""
+        """Examine the movements of the bishop located on square (1, 3) - Check test."""
         self.assertEqual(self.get_moves(1, 3), ({(3, 5)}, {(3, 1)}))
 
     def test_check_queen_7_1(self):
-        """Examine the movements of the queen located on square (7, 1)."""
+        """Examine the movements of the queen located on square (7, 1) - Check test."""
         self.assertEqual(self.get_moves(1, 3), ({(3, 5)}, {(3, 1)}))
 
     def test_check_king_3_6(self):
-        """Examine the movements of the queen located on square (3, 6)."""
+        """Examine the movements of the king located on square (3, 6) - Check test."""
         self.assertEqual(self.get_moves(3, 6), ({(2, 5), (2, 6), (2, 7), (4, 5), (4, 6), (4, 7)}, set()))
 
 class DoubleCheck(unittest.TestCase):
@@ -276,7 +276,7 @@ class DoubleCheck(unittest.TestCase):
     def setUpClass(cls):
         cls.board = [[None for _ in range(ROWS)] for _ in range(COLS)]
         cls.possible_moves = [[None for _ in range(ROWS)] for _ in range(COLS)]
-        cls.turn, cls.opponents_attacks, cls.checkin_pieces, cls.pinned_pieces = "black", set(), {}, {}
+        cls.turn, cls.opponents_attacks, cls.checkin_pieces, cls.pinned_pieces, cls.castling, cls.enpassant = "black", set(), {}, {}, "____", None
 
         cls.pieces_positions = [("pawn", "black", (4, 0)), ("pawn", "black", (4, 2)), ("pawn", "black", (3, 7)),
                                 ("rook", "white", (0, 6)), ("rook", "black", (7, 5)),
@@ -305,39 +305,39 @@ class DoubleCheck(unittest.TestCase):
                     cls.possible_moves[row][col] = cls.get_moves(cls, row, col)
     
     def get_moves(self, row, col):
-        moves, attacks = self.board[row][col].check_piece_possible_moves(self, self.opponents_attacks, self.checkin_pieces, self.pinned_pieces)
+        moves, attacks = self.board[row][col].check_piece_possible_moves(self, self.opponents_attacks, self.checkin_pieces, self.pinned_pieces, self.castling, self.enpassant)
         return (set(moves), set(attacks))
 
     def test_double_check_pawn_4_0(self):
-        """Examine the movements of the pawn located on square (4, 0)."""
+        """Examine the movements of the pawn located on square (4, 0) - Double check test."""
         self.assertEqual(self.get_moves(4, 0), (set(), set()))
 
     def test_double_check_pawn_4_2(self):
-        """Examine the movements of the pawn located on square (4, 2)."""
+        """Examine the movements of the pawn located on square (4, 2) - Double check test."""
         self.assertEqual(self.get_moves(4, 2), (set(), set()))
 
     def test_double_check_pawn_3_7(self):
-        """Examine the movements of the pawn located on square (3, 7)."""
+        """Examine the movements of the pawn located on square (3, 7) - Double check test."""
         self.assertEqual(self.get_moves(3, 7), (set(), set()))
 
     def test_double_check_rook_7_5(self):
-        """Examine the movements of the rook located on square (7, 5)."""
+        """Examine the movements of the rook located on square (7, 5) - Double check test."""
         self.assertEqual(self.get_moves(7, 5), (set(), set()))
 
     def test_double_check_knight_1_2(self):
-        """Examine the movements of the knight located on square (1, 2)."""
+        """Examine the movements of the knight located on square (1, 2) - Double check test."""
         self.assertEqual(self.get_moves(1, 2), (set(), set()))
 
     def test_double_check_bishop_1_3(self):
-        """Examine the movements of the bishop located on square (1, 3)."""
+        """Examine the movements of the bishop located on square (1, 3) - Double check test."""
         self.assertEqual(self.get_moves(1, 3), (set(), set()))
 
     def test_double_check_queen_7_1(self):
-        """Examine the movements of the queen located on square (7, 1)."""
+        """Examine the movements of the queen located on square (7, 1) - Double check test."""
         self.assertEqual(self.get_moves(1, 3), (set(), set()))
         
     def test_check_king_3_6(self):
-        """Examine the movements of the queen located on square (3, 6)."""
+        """Examine the movements of the king located on square (3, 6) - Double check test."""
         self.assertEqual(self.get_moves(3, 6), ({(2, 5), (2, 7), (4, 5), (4, 7)}, set()))
 
 if __name__ == '__main__':
