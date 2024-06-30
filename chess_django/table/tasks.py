2733c8434ab7f586aba2b7d865dcdb0d6ae883d0
Przekazmierczak <prze.kazmierczak@gmail.com>
2024-06-30 12:47:01 +0200

    add comments to tasks.py

--- chess_django/table/tasks.py
+++ chess_django/table/tasks.py
@@ -8,10 +8,14 @@ def check_game_timeout(game_id, turn, total_moves, board_json):
     turn = "w" if turn == "white" else "b"
     game = Game.objects.get(pk=game_id)
     board = Board.objects.filter(game=game).latest('id')
+
+    # Check if the current board state matches the expected turn and move count
     if board.turn == turn and board.total_moves == total_moves and game.winner == None:
+        # Get the remaining time for both players
         white_time_left = board.white_time_left.total_seconds()
         black_time_left = board.black_time_left.total_seconds()
 
+        # Determine if the game has timed out for the current player
         if board.turn == "w":
             game.winner = "b"
             winner = "black"
@@ -20,8 +24,11 @@ def check_game_timeout(game_id, turn, total_moves, board_json):
             game.winner = "w"
             winner = "white"
             black_time_left = 0
+        
+        # Save the game state with the winner
         game.save()
 
+        # Construct the message with the updated game state
         message = {
             "white_player": game.white.username,
             "black_player": game.black.username,
@@ -37,6 +44,7 @@ def check_game_timeout(game_id, turn, total_moves, board_json):
             "black_time_left": black_time_left
         }
 
+        # Get the channel layer to send a message to the room group
         channel_layer = get_channel_layer()
         async_to_sync(channel_layer.group_send)(
             f'table_{game.id}',
