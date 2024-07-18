11ba1a8662bcbcfd45b501c2f71f5fef6161b50d
Przekazmierczak <prze.kazmierczak@gmail.com>
2024-07-18 22:04:48 +0200

    add table for free games and navigation links to lobby_interaction.js

--- chess_django/lobby/static/lobby/lobby_interaction.js
+++ chess_django/lobby/static/lobby/lobby_interaction.js
@@ -3,4 +3,46 @@ const lobbySocket = new WebSocket(`ws://${window.location.host}/ws/lobby/`);
 lobbySocket.onmessage = function(e) {
     const state = JSON.parse(e.data);
     console.log(state)
+
+    const freeGamesContainer = document.getElementById('free-games');
+    const games = createTable(state.free_games);
+    freeGamesContainer.appendChild(games);
+    
 };
+
+function createTable(data) {
+    const table = document.createElement('table');
+
+    // Loop through each row in the data array
+    data.forEach(gameData => {
+        const gameRow = document.createElement('tr');
+
+        // gameRow.id = 'game-' + gameData[0];
+
+        gameRow.addEventListener('click', () => {
+            window.location.href = `table/${gameData[0]}/`;
+        });
+
+        const game_id = document.createElement('tr');
+        game_id.innerText = 'Table: ' + gameData[0];
+        
+        gameRow.appendChild(game_id);
+        table.appendChild(gameRow);
+
+        const playersRow = document.createElement('tr');
+        
+        const white_player = document.createElement('td');
+        white_player.innerText = 'White: ' + gameData[1];
+        playersRow.appendChild(white_player);
+        
+        const black_player = document.createElement('td');
+        black_player.innerText = 'Black: ' + gameData[2];
+        playersRow.appendChild(black_player);
+
+        gameRow.appendChild(playersRow);
+
+        table.appendChild(gameRow);
+    });
+
+    return table;
+}
--- chess_django/lobby/views.py
+++ chess_django/lobby/views.py
@@ -6,10 +6,4 @@ from table.models import Game
 # Create your views here.
 
 def index(request):
-    free_tables = Game.objects.filter(Q(white_ready=False) | Q(black_ready=False)).order_by('-id')[:10]
-    for free_table in free_tables:
-        test = free_table.id
-
-    return render(request, "lobby/index.html", {
-        "free_tables": test,
-    })
+    return render(request, "lobby/index.html")
