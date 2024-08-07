f98482cb19e882cc64a0c26636ab923b5f06fb28
Przekazmierczak <prze.kazmierczak@gmail.com>
2024-08-07 21:40:47 +0200

    add GameWithAi in table.models and redirest to table with easy AI

--- chess_django/play_with_computer/urls.py
+++ chess_django/play_with_computer/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="computer_index"),
+    path("create_easy_game", views.create_easy_game, name="create_easy_game"),
 ]
--- chess_django/play_with_computer/views.py
+++ chess_django/play_with_computer/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from table.models import GameWithAi
+from menu.models import User
 
 # Create your views here.
 def index(request):
-    return render(request, "computer/index.html")
+    return render(request, "play_with_computer/index.html")
+
+def create_easy_game(request):
+    new_game = GameWithAi.objects.create()
+
+    easy_player = User.objects.get(username="Easy_Computer")
+    new_game.black = easy_player
+    new_game.black_ready = True
+    new_game.save()
+
+    table_id = f"ai{new_game.id}"
+    return redirect("table", table_id=table_id)
