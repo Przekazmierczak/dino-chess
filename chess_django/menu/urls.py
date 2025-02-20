7ad0333711c6dd5a7669f47ef43daa14045ee25d
Przekazmierczak <prze.kazmierczak@gmail.com>
2025-02-20 23:43:21 +0100

    Implement user interface for game history

--- chess_django/menu/urls.py
+++ chess_django/menu/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.index, name="menu_index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("user", views.user, name="user")
 ]
--- chess_django/menu/views.py
+++ chess_django/menu/views.py
@@ -1,8 +1,13 @@
+from datetime import datetime, timezone
+
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.db import IntegrityError
+
+from table.models import Game
+from django.db.models import Q
 
 from .models import User
 
@@ -41,9 +46,15 @@ def register(request):
         email = request.POST["email"]
 
         # Ensure username is enough length
-        if (len(username) <= 5):
+        if len(username) <= 5:
             return render(request, "menu/register.html", {
                 "message": "Username is too short."
+            })
+        
+        # Ensure username is not too long
+        if len(password) >= 20:
+            return render(request, "menu/register.html", {
+                "message": "Username is too long."
             })
         
         # Ensure password matches confirmation
@@ -59,7 +70,7 @@ def register(request):
             return render(request, "menu/register.html", {
                 "message": "Passwords is too short."
             })
-
+        
         # Attempt to create new user
         try:
             user = User.objects.create_user(username, email, password)
@@ -72,3 +83,23 @@ def register(request):
         return HttpResponseRedirect(reverse("menu_index"))
     else:
         return render(request, "menu/register.html")
+    
+def user(request):
+    user = request.user
+    userGames = list(Game.objects.filter((Q(white=user) | Q (black=user)) & Q(finished_at__isnull=False)).order_by('-finished_at'))
+    for i in range(len(userGames)):
+        userGames[i].finished_at = int(userGames[i].finished_at.timestamp() * 1000)
+        
+        if userGames[i].winner == "w":
+            if user == userGames[i].white:
+                userGames[i].winner = "w"
+            else:
+                userGames[i].winner = "l"
+        if userGames[i].winner == "b":
+            if user == userGames[i].white:
+                userGames[i].winner = "l"
+            else:
+                userGames[i].winner = "w"
+        
+    games = {'games': userGames}
+    return render(request, "menu/user.html", games)
